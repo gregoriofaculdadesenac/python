22883607b7b215689bc505dcde299038f235ee37
gregoriofaculdadesenac <117416204+gregoriofaculdadesenac@users.noreply.github.com>
2022-11-03 22:03:12 -0300

    Add files via upload

---
+++ exer01.py
@@ -0,0 +1,7 @@
+#caracter de comentario
+#print para imprimir informações na tela 
+
+
+
+print('olá mundo python')
+print('meu nome é','Gregório',44,'Anos')
---
+++ exer02.py
@@ -0,0 +1,3 @@
+#usando input para entrada de dados
+print(' Seja Bem - Vindo a aula de' + 'Python')
+print('Ola {}' . format(input('Qual é o seu nome')))
